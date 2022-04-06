2c88b93e49533618e607235f3850bc5004e05c98
VovaFet <f.vladimir97@gmail.com>
2022-04-07 01:02:09 +0300

    all in one json

--- links_generator.py
+++ links_generator.py
@@ -6,22 +6,20 @@ headers = {
     "User-Agent": generate_user_agent()
 }
 def links_generator():
-    # global url
+
     generated_url_list = []
     req = requests.get(url, headers=headers).text
     soup = BeautifulSoup(req, "lxml")
     page_number = soup.find_all("a", class_="block br3 brc8 large tdnone lheight24")
     if page_number==[]:
         generated_url = url
-        # print(generated_url)
     else:
         for _ in page_number:
             page = int(_.find("span").text)
-        # print(page)
         for gg in range(1,page+1):
             generated_url = url+'?page='+str(gg)
             generated_url_list.append(generated_url)
-        print(generated_url_list)
+        # print(generated_url_list)
     return(generated_url_list)
 links_generator()
 
