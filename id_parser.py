92567f03b74b07fcd17d5d02b0ddf08b3154c88f
VovaFet <f.vladimir97@gmail.com>
2022-04-08 18:48:05 +0300

    Parsing id from ads page

---
+++ id_parser.py
@@ -0,0 +1,36 @@
+import requests
+from bs4 import BeautifulSoup
+from time import sleep
+import time
+from os import system
+import json
+from user_agent import generate_user_agent
+# from selenium import webdriver
+
+url = 'https://www.olx.ua/d/obyavlenie/sdam-na-1-2-mesyatsa-1-k-kvartiru-metro-levobezhnaya-bez-komissii-IDOnMxS.html#5291a2c816'
+headers = {
+"User-Agent": generate_user_agent()}
+# req = requests.get(url, headers = headers).text
+def id_extractor(url):
+    try:
+        # html = open('test.html', 'r')
+        req = requests.get(url, headers = headers).text
+        soup = BeautifulSoup(req, "lxml")
+        id_number = soup.find("span", class_="css-9xy3gn-Text eu5v0x0")
+        id_number = id_number.text
+        
+        
+        print('--------------')
+        # print(id_number[-9:-1:1])
+        print(id_number)
+        
+    except(AttributeError):
+        # id_extractor(url)
+        print('XXAXX')
+
+if __name__ == "__main__":
+    while True:
+        # generated_url_list = links_generator()
+        # for url in generated_url_list:
+        id_extractor(url)
+ 
